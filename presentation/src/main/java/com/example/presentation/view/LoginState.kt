3da000a057d30911776e07668266e649253cb880
carlos iniesta <ciniesta@everis.com>
2020-02-05 15:57:19 +0100

    State control for LoginFragment

--- presentation/src/main/java/com/example/presentation/view/LoginState.kt
+++ presentation/src/main/java/com/example/presentation/view/LoginState.kt
@@ -1,7 +1,15 @@
 package com.example.presentation.view
 
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
 sealed class LoginState: Parcelable {
+    @Parcelize
+    object SignInOk : LoginState()
 
+    @Parcelize
+    object SignInError : LoginState()
+
+    @Parcelize
+    object NotReadyToSignIn : LoginState()
 }
